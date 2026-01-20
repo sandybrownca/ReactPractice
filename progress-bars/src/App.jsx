433a8199df8a1ef47fca8d63089d37ce7b0720c9
Sandeep Joshi <ysandeepjoshi@gmail.com>
2026-01-19 22:01:52 -0500

    Auto fill Progress bar

---
+++ progress-bars/src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [progressBar, setProgressBar] = useState([]);
+
+  function addProgressBar() {
+    const id = Date.now()
+    const progressItem = {
+      progressBarId: id,
+      value: 0
+    }
+    setProgressBar(prev => [
+      ...prev,
+      { progressBarId: id, value: 0 }
+    ]);
+
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgressBar(prev =>
+        prev.map(bar => {
+          if (bar.value >= 100) return bar;
+          return {
+            ...bar,
+            value: Math.min(bar.value + 10, 100)
+          };
+
+        })
+      );
+    }, 200);
+
+  }, [])
+  return (
+    <div>
+      <button onClick={() => { addProgressBar() }}>Add</button>
+
+      {
+        progressBar.map((item) => {
+          return <div>
+            <progress id={item.progressBarId} value={item.value} max="100"> 30% </progress>
+          </div>
+        })
+      }
+    </div>
+  );
+}
